22c9f31587d0316626ac58ec1bca49d22fd96668
Krzysztof Szumny <noisy.pl@gmail.com>
2024-03-28 01:38:07 +0100

    Support for 'rotate_selection: bool' in InteractiveTable. More tests

--- rich_interactive/interactive_table.py
+++ rich_interactive/interactive_table.py
@@ -6,14 +6,20 @@ from rich_interactive.interactive import Interactive
 
 
 class InteractiveTable(Interactive, Table):
-    selected_row = 0
+    selected_row: int | None = None
     is_selected: bool = False
+    rotate_selection: bool = False
+
+    def __init__(self, selected_row=0, rotate_selection=False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.selected_row = selected_row
+        self.rotate_selection = rotate_selection
 
     def get_row_style(self, console: Console, index: int) -> StyleType:
         style = super().get_row_style(console, index)
 
         if index == self.selected_row:
-            style = style + Style(bgcolor="bright_red", color="white")
+            style = style + Style(bgcolor="bright_black", color="white")
 
         return style
 
@@ -22,9 +28,21 @@ class InteractiveTable(Interactive, Table):
         return not self.layout or self.layout.is_selected
 
     def move_selection_down(self):
+        if self.selected_row is None and len(self.rows) > 0:
+            self.selected_row = 0
+
         if self.selected_row < len(self.rows) - 1:
             self.selected_row += 1
 
+        if self.selected_row == len(self.rows) - 1 and self.rotate_selection:
+            self.selected_row = 0
+
     def move_selection_up(self):
+        if self.selected_row is None and len(self.rows) > 0:
+            self.selected_row = len(self.rows) - 1
+
         if self.selected_row > 0 and len(self.rows) > 0:
             self.selected_row -= 1
+
+        if self.selected_row == 0 and self.rotate_selection:
+            self.selected_row = len(self.rows) - 1
--- tests/test_interactive_table.py
+++ tests/test_interactive_table.py
@@ -3,25 +3,203 @@ from tests.utils import adjust_output as _
 from tests.utils import diff_output
 
 
-def test_interactive_table_default_row_selection(render_to_text):
-    table = InteractiveTable(title="Interactive Table")
+def add_columns_and_rows(table, rows=3):
     table.add_column("#")
     table.add_column("Column 1", width=12)
-    table.add_row("1", "Row 1")
-    table.add_row("2", "Row 2")
-    table.add_row("3", "Row 3")
+    for i in range(1, rows + 1):
+        table.add_row(str(i), f"Row {i}")
+
+
+def test_interactive_table_default_row_selection(render_to_text):
+    table = InteractiveTable(title="Interactive Table")
+    add_columns_and_rows(table)
 
     expected_output = _("""
     [3m Interactive Table  [0m
     ┏━━━┳━━━━━━━━━━━━━━┓
     ┃[1m [0m[1m#[0m[1m [0m┃[1m [0m[1mColumn 1    [0m[1m [0m┃
     ┡━━━╇━━━━━━━━━━━━━━┩
-    │[37;101m [0m[37;101m1[0m[37;101m [0m│[37;101m [0m[37;101mRow 1       [0m[37;101m [0m│
+    │[37;100m [0m[37;100m1[0m[37;100m [0m│[37;100m [0m[37;100mRow 1       [0m[37;100m [0m│
     │ 2 │ Row 2        │
     │ 3 │ Row 3        │
     └───┴──────────────┘
     """)
 
     output = render_to_text(table)
+    diff_output(output, expected_output)
 
+
+def test_interactive_table_no_selected_row(render_to_text):
+    table = InteractiveTable(title="Interactive Table", selected_row=None)
+    add_columns_and_rows(table)
+
+    expected_output = _("""
+    [3m Interactive Table  [0m
+    ┏━━━┳━━━━━━━━━━━━━━┓
+    ┃[1m [0m[1m#[0m[1m [0m┃[1m [0m[1mColumn 1    [0m[1m [0m┃
+    ┡━━━╇━━━━━━━━━━━━━━┩
+    │ 1 │ Row 1        │
+    │ 2 │ Row 2        │
+    │ 3 │ Row 3        │
+    └───┴──────────────┘
+    """)
+
+    output = render_to_text(table)
+    diff_output(output, expected_output)
+
+
+def test_interactive_table_selected_row(render_to_text):
+    table = InteractiveTable(title="Interactive Table", selected_row=1)
+    add_columns_and_rows(table)
+
+    expected_output = _("""
+    [3m Interactive Table  [0m
+    ┏━━━┳━━━━━━━━━━━━━━┓
+    ┃[1m [0m[1m#[0m[1m [0m┃[1m [0m[1mColumn 1    [0m[1m [0m┃
+    ┡━━━╇━━━━━━━━━━━━━━┩
+    │ 1 │ Row 1        │
+    │[37;100m [0m[37;100m2[0m[37;100m [0m│[37;100m [0m[37;100mRow 2       [0m[37;100m [0m│
+    │ 3 │ Row 3        │
+    └───┴──────────────┘
+    """)
+
+    output = render_to_text(table)
+    diff_output(output, expected_output)
+
+
+def test_interactive_table_selected_row_moved_down(render_to_text):
+    table = InteractiveTable(title="Interactive Table", selected_row=1)
+    add_columns_and_rows(table)
+
+    table.move_selection_down()
+
+    expected_output = _("""
+    [3m Interactive Table  [0m
+    ┏━━━┳━━━━━━━━━━━━━━┓
+    ┃[1m [0m[1m#[0m[1m [0m┃[1m [0m[1mColumn 1    [0m[1m [0m┃
+    ┡━━━╇━━━━━━━━━━━━━━┩
+    │ 1 │ Row 1        │
+    │ 2 │ Row 2        │
+    │[37;100m [0m[37;100m3[0m[37;100m [0m│[37;100m [0m[37;100mRow 3       [0m[37;100m [0m│
+    └───┴──────────────┘
+    """)
+
+    output = render_to_text(table)
+    diff_output(output, expected_output)
+
+
+def test_interactive_table_selected_row_moved_up(render_to_text):
+    table = InteractiveTable(title="Interactive Table", selected_row=2)
+    add_columns_and_rows(table)
+
+    table.move_selection_up()
+
+    expected_output = _("""
+    [3m Interactive Table  [0m
+    ┏━━━┳━━━━━━━━━━━━━━┓
+    ┃[1m [0m[1m#[0m[1m [0m┃[1m [0m[1mColumn 1    [0m[1m [0m┃
+    ┡━━━╇━━━━━━━━━━━━━━┩
+    │ 1 │ Row 1        │
+    │[37;100m [0m[37;100m2[0m[37;100m [0m│[37;100m [0m[37;100mRow 2       [0m[37;100m [0m│
+    │ 3 │ Row 3        │
+    └───┴──────────────┘
+    """)
+
+    output = render_to_text(table)
+    diff_output(output, expected_output)
+
+
+def test_interactive_table_last_selected_row_moved_down_does_not_move_selection(
+    render_to_text,
+):
+    table = InteractiveTable(title="Interactive Table", selected_row=2)
+    add_columns_and_rows(table)
+
+    table.move_selection_down()
+
+    expected_output = _("""
+    [3m Interactive Table  [0m
+    ┏━━━┳━━━━━━━━━━━━━━┓
+    ┃[1m [0m[1m#[0m[1m [0m┃[1m [0m[1mColumn 1    [0m[1m [0m┃
+    ┡━━━╇━━━━━━━━━━━━━━┩
+    │ 1 │ Row 1        │
+    │ 2 │ Row 2        │
+    │[37;100m [0m[37;100m3[0m[37;100m [0m│[37;100m [0m[37;100mRow 3       [0m[37;100m [0m│
+    └───┴──────────────┘
+    """)
+
+    output = render_to_text(table)
+    diff_output(output, expected_output)
+
+
+def test_interactive_table_first_selected_row_moved_up_does_not_move_selection(
+    render_to_text,
+):
+    table = InteractiveTable(title="Interactive Table", selected_row=0)
+    add_columns_and_rows(table)
+
+    table.move_selection_up()
+
+    expected_output = _("""
+    [3m Interactive Table  [0m
+    ┏━━━┳━━━━━━━━━━━━━━┓
+    ┃[1m [0m[1m#[0m[1m [0m┃[1m [0m[1mColumn 1    [0m[1m [0m┃
+    ┡━━━╇━━━━━━━━━━━━━━┩
+    │[37;100m [0m[37;100m1[0m[37;100m [0m│[37;100m [0m[37;100mRow 1       [0m[37;100m [0m│
+    │ 2 │ Row 2        │
+    │ 3 │ Row 3        │
+    └───┴──────────────┘
+    """)
+
+    output = render_to_text(table)
+    diff_output(output, expected_output)
+
+
+def test_interactive_table_last_selected_row_moved_down_when_rotate_selection_true_does_move_selection_to_first_row(
+    render_to_text,
+):
+    table = InteractiveTable(
+        title="Interactive Table", selected_row=2, rotate_selection=True
+    )
+    add_columns_and_rows(table)
+
+    table.move_selection_down()
+
+    expected_output = _("""
+    [3m Interactive Table  [0m
+    ┏━━━┳━━━━━━━━━━━━━━┓
+    ┃[1m [0m[1m#[0m[1m [0m┃[1m [0m[1mColumn 1    [0m[1m [0m┃
+    ┡━━━╇━━━━━━━━━━━━━━┩
+    │[37;100m [0m[37;100m1[0m[37;100m [0m│[37;100m [0m[37;100mRow 1       [0m[37;100m [0m│
+    │ 2 │ Row 2        │
+    │ 3 │ Row 3        │
+    └───┴──────────────┘
+    """)
+
+    output = render_to_text(table)
+    diff_output(output, expected_output)
+
+
+def test_interactive_table_first_selected_row_moved_up_when_rotate_selection_true_does_move_selection_to_last_row(
+    render_to_text,
+):
+    table = InteractiveTable(
+        title="Interactive Table", selected_row=0, rotate_selection=True
+    )
+    add_columns_and_rows(table)
+
+    table.move_selection_up()
+
+    expected_output = _("""
+    [3m Interactive Table  [0m
+    ┏━━━┳━━━━━━━━━━━━━━┓
+    ┃[1m [0m[1m#[0m[1m [0m┃[1m [0m[1mColumn 1    [0m[1m [0m┃
+    ┡━━━╇━━━━━━━━━━━━━━┩
+    │ 1 │ Row 1        │
+    │ 2 │ Row 2        │
+    │[37;100m [0m[37;100m3[0m[37;100m [0m│[37;100m [0m[37;100mRow 3       [0m[37;100m [0m│
+    └───┴──────────────┘
+    """)
+
+    output = render_to_text(table)
     diff_output(output, expected_output)
